88fecdba211262feab78a41cba502605ed5685c7
mangeishp <mangeish.patil@teliacompany.com>
2021-04-20 10:51:16 +0200

    patchmanger

--- example/main.tf
+++ example/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
 module "my-inspector-deployment" {
   source = "../modules/inspector"
   #version                         = "~> 3.0"
@@ -10,8 +14,50 @@ module "my-inspector-deployment" {
   ruleset_network_reachability    = false
 }
 
+module "patch_baseline_amazonlinux2" {
+  source = "../modules/patchmanager"
 
+  # tags parameters
+  environment = "dev"
 
-provider "aws" {
-  region = "eu-north-1"
+  # patch baseline parameters
+  approved_patches_compliance_level = "HIGH"
+  operating_system                  = local.operating_system_amazon_linux2
+  description                       = "AmazonLinux2 - PatchBaseLine - Apply Critical Security Updates"
+  tags                              = var.tags
+
+  # define rules inside patch baseline
+  patch_baseline_approval_rules = [
+    {
+      approve_after_days  = 7
+      compliance_level    = "CRITICAL"
+      enable_non_security = false
+      patch_baseline_filters = [
+        {
+          name   = "PRODUCT"
+          values = ["AmazonLinux2", "AmazonLinux2.0"]
+        },
+        {
+          name   = "CLASSIFICATION"
+          values = ["Security"]
+        },
+        {
+          name   = "SEVERITY"
+          values = ["Critical"]
+        }
+      ]
+    }
+  ]
+
+  # parameters for scan : associated patch_group "scan" to this patch baseline
+  enable_mode_scan = true
+}
+
+# register as default patch baseline our patch baseline
+module "register_patch_baseline_amazonlinux2" {
+  source = "../modules/register_default_patch_baseline"
+
+  set_default_patch_baseline = true
+  patch_baseline_id          = module.patch_baseline_amazonlinux2.patch_baseline_id
+  operating_system           = local.operating_system_amazon_linux2
 }
